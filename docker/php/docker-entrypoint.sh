b6c3e7edd5040e1e3dc889b9122ca886e331b5ba
Anita Molnár <anita.molnar1993@gmail.com>
2025-04-21 16:24:22 +0200

    Add php.ini path to entrypoint

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+	set -- php-fpm -c "$PHP_INI_DIR/php.ini" "$@"
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
